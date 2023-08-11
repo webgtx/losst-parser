440c6f7ba14e4a34afadae1cceb9284a1c5c4306
webgtx <alx.zolotarov@gmail.com>
2023-08-11 23:29:14 +0000

    Fixes: css selector was updated & added .gitignore

--- func.py
+++ func.py
@@ -2,9 +2,9 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 
 def parse(url, target = None):
-    request = req.get(url)
-    soup = bs(request.text, 'html.parser')
-    themeLinks = soup.find_all('h2', class_='title post-title')
+    res = req.get(url)
+    soup = bs(res.text, 'html.parser')
+    themeLinks = soup.find_all('h2', class_='entry-title')
 
     for link in themeLinks:
         link = link.find('a')
